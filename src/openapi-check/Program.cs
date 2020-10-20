d29875530eac3c503d1a8ea50b5b8ed67b78a22c
suremaker <wojciech.kotlarski@gmail.com>
2020-10-20 20:10:52 +0100

    Updated Program.cs to use validation

--- src/openapi-check/Program.cs
+++ src/openapi-check/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.OpenApi.Models;
@@ -11,15 +12,44 @@ namespace OpenApiCheck
     {
         static async Task<int> Main(string[] args)
         {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    await Console.Error.WriteLineAsync($"Invalid parameter count: {args.Length}");
+                    await Console.Error.WriteLineAsync("Usage: openapi-check [old-swagger-uri] [new-swagger-uri]");
+                    return -1;
+                }
+
+                return await CompareSwaggers(args);
+            }
+            catch (Exception ex)
+            {
+                await Console.Error.WriteLineAsync($"Application failed with unexpected error:\n{ex}");
+                return -1;
+            }
+        }
+
+        private static async Task<int> CompareSwaggers(string[] args)
+        {
             var swagger1 = await ReadSwagger(args[0]);
             var swagger2 = await ReadSwagger(args[1]);
 
-            return 0;
+            var result = OpenApiComparer.Compare(swagger1, swagger2);
+            foreach (var operation in result.Operations)
+            {
+                Console.WriteLine($"{operation.FullName}: {operation.Status}");
+                foreach (var message in operation.Messages)
+                    Console.WriteLine($"  {message.Status}: {message.Message}");
+            }
+
+            return (int)result.Status;
         }
 
         private static async Task<OpenApiDocument> ReadSwagger(string path)
         {
-            if (new Uri(path).IsFile)
+            var uri = new Uri(path, UriKind.RelativeOrAbsolute);
+            if (!uri.IsAbsoluteUri || uri.IsFile)
             {
                 await using var stream = File.OpenRead(path);
                 return new OpenApiStreamReader().Read(stream, out _);
